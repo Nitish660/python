67bd22734f5695ef89370cd1b076723578444630
20051742@kiit.ac.in <20051742@kiit.ac.in>
2023-08-07 00:34:04 +0530

    python1

---
+++ loop.py
@@ -0,0 +1,6 @@
+#the provided code stub reads and integer, , from STDIN. For all non-negative integers , print .
+if __name__ == '__main__':
+    N = int(input())
+    
+    for i in range(0,N):
+        print (i * i)
